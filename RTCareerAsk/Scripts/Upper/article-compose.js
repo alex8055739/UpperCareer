22da592f7ae4db073208d8d63e19f9a24fa3c5c3
alex8055739 <42379442@qq.com>
2016-11-16 12:50:51 +0800

    Bug fixed
    Fixed the issue that cause the quality drop of article cover pictures.

--- RTCareerAsk/Scripts/Upper/article-compose.js
+++ RTCareerAsk/Scripts/Upper/article-compose.js
@@ -79,8 +79,6 @@ $(document).ready(function () {
                     btn.removeClass('disabled').text(btnText);
                 }
             })
-        },
-        'image/jpeg'
-        )
+        })
     })
 })
